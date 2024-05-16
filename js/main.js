e4dab02c0b07a5dc086bcfdebe1bc678da59fc36
Konstantin Krishtapov <115942824+KonstanLife@users.noreply.github.com>
2024-05-16 13:51:48 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,15 @@
+const menuBtn = document.querySelector(".menu__btn");
+const menuMobile = document.querySelector(".header__menu--list");
+
+menuBtn.addEventListener("click", () => {
+  menuMobile.classList.toggle("menu--open");
+});
+
+const swiperOne = new Swiper(".feedback__slider", {
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
